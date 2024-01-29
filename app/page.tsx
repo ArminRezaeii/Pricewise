44984a318ce38f37db8b5207837ce69c01de39ab
ArminRezaei <arminrezaei53@gmail.com>
2024-01-29 19:25:54 +0330

    Home Page Basic Setup Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,41 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <section className="px-6 border-2 md:px-20 py-24 border-red-500">
+        <div className="flex max-xl:flex-col gap-6">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary">PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+            Searchbar
+          </div>
+          HeroCarousel
+        </div>
+      </section>
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+{['Apple Iphone','asusProduct','sumsung'].map((product)=>(
+  <div>{product}</div>
+))}
+        </div>
+      </section>
+    </>
+  );
 }
